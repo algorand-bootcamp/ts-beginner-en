0493eca93d32a3209c8d8a4bf8efd18db7c31366
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2023-09-12 19:20:18 +0200

    Add a voting token.

--- contracts/dao.algo.ts
+++ contracts/dao.algo.ts
@@ -2,6 +2,8 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class Dao extends Contract {
+  registeredAsaId = GlobalStateKey<Asset>();
+
   proposal = GlobalStateKey<string>({ key: 'p' });
 
   votesTotal = GlobalStateKey<number>();
@@ -12,7 +14,39 @@ class Dao extends Contract {
     this.proposal.value = proposal;
   }
 
-  vote(inFavor: boolean): void {
+  bootstrap(): Asset {
+    assert(this.txn.sender === this.app.creator);
+    assert(!this.registeredAsaId.exists);
+    const registeredAsa = sendAssetCreation({
+      configAssetTotal: 1_000,
+      configAssetDecimals: 0,
+      configAssetFreeze: this.app.address,
+      fee: 0,
+    });
+    this.registeredAsaId.value = registeredAsa;
+    return registeredAsa;
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  register(registeredASA: Asset): void {
+    assert(this.txn.sender.assetBalance(this.registeredAsaId.value) === 0);
+    sendAssetTransfer({
+      xferAsset: this.registeredAsaId.value,
+      assetReceiver: this.txn.sender,
+      assetAmount: 1,
+      fee: 0,
+    });
+    sendAssetFreeze({
+      freezeAsset: this.registeredAsaId.value,
+      freezeAssetAccount: this.txn.sender,
+      freezeAssetFrozen: true,
+      fee: 0,
+    });
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  vote(inFavor: boolean, registeredASA: Asset): void {
+    assert(this.txn.sender.assetBalance(this.registeredAsaId.value) === 1);
     this.votesTotal.value = this.votesTotal.value + 1;
     if (inFavor) {
       this.votesInFavor.value = this.votesInFavor.value + 1;
@@ -21,6 +55,11 @@ class Dao extends Contract {
 
   getProposal(): string {
     return this.proposal.value;
+  }
+
+  getRegisteredASA(): Asset {
+    assert(this.registeredAsaId.exists);
+    return this.registeredAsaId.value;
   }
 
   getVotes(): { votesTotal: number, votesInFavor: number } {
