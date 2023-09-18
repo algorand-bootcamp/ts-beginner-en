f92870de27c8b75f264a68f6f79c5e18be2b6a27
Joe Polny <joepolny@gmail.com>
2023-09-18 17:24:13 -0400

    update TEALScript v101

--- contracts/dao.algo.ts
+++ contracts/dao.algo.ts
@@ -2,7 +2,7 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class Dao extends Contract {
-   /**
+  /**
    * Calculates the sum of two numbers
    *
    * @param a
